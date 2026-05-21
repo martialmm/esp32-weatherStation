3abce3394b8f40e448c685b740c276d6f2b206ca
Mars <petitmonne39@hotmail.fr>
2026-05-21 22:53:11 +0200

    BLUETOOTH: start to implement nimBLE bluetooth by initializing it and showing a text confirming it is running

--- main/main.c
+++ main/main.c
@@ -4,6 +4,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h" 
 
+// BLUETOOTH
+#include "nvs_flash.h"
+#include "nimble/nimble_port.h"
+#include "nimble/nimble_port_freertos.h"
+
 QueueHandle_t xQueue = NULL;
 TaskHandle_t xTaskTemperatureHandle = NULL;
 TaskHandle_t xTaskHumidityHandle = NULL;
@@ -45,6 +50,16 @@ void vTaskSendTemperature(void* pvParameters)
             ESP_LOGI("error", "temperature value could not be sent!");
         }
     }
+}
+
+void nimble_host_task(void *param) {
+    ESP_LOGI("BLE", "NimBLE Running...");
+
+    /* Loops until nimble_port_stop() is executed */
+    nimble_port_run();
+    
+    /* Clean up at exit */
+    vTaskDelete(NULL);
 }
 
 void vTaskSendHumidity(void* pvParameters)
@@ -89,8 +104,10 @@ void app_main(void)
 {
     TimerHandle_t xTimerForTemperature = xTimerCreate("Timer temp sensor", pdMS_TO_TICKS(1000), pdTRUE, NULL, xTaskTemperatureCallback);
     TimerHandle_t xTimerForHumidity = xTimerCreate("Timer humidity sensor", pdMS_TO_TICKS(1000), pdTRUE, NULL, xTaskHumidityCallback);
-
     xQueue = xQueueCreate(20, sizeof(SensorDatas_t));
+
+    // BLUETOOTH variables
+    esp_err_t objectInitStatus = ESP_OK;
 
     if (xTimerStart(xTimerForTemperature, pdMS_TO_TICKS(100)) != pdPASS) {
         ESP_LOGE("temperature sensor", "Failed to start timer");
@@ -101,6 +118,32 @@ void app_main(void)
         ESP_LOGE("humidity sensor", "Failed to start timer");
         return;
     }
+
+    /* NVS flash initialization */
+    /* --> initialize a part of the flash memory for the bluetooth stack 
+       --> if this init is not successfull, we erase the NVS part and we redo an init*/
+    objectInitStatus = nvs_flash_init();
+    if (objectInitStatus == ESP_ERR_NVS_NO_FREE_PAGES || objectInitStatus == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        objectInitStatus = nvs_flash_init();
+    }
+
+    // if the init still fails, we stop the program:
+    if (objectInitStatus != ESP_OK) {
+        ESP_LOGE("NVS", "failed to initialize nvs flash, error code: %d ", objectInitStatus);
+        return;
+    }
+
+    /* NimBLE host stack initialization */
+    objectInitStatus = nimble_port_init();
+    if (objectInitStatus != ESP_OK) {
+        ESP_LOGE("NimBLE", "failed to initialize nimble stack, error code: %d ", objectInitStatus);
+        return;
+    }
+
+    // Create Host Bluetooth freeRTOS task
+    xTaskCreate(nimble_host_task, "ble_host", 4096, NULL, 5, NULL);
+
 
     if(xQueue != NULL){
         xTaskCreate(vTaskSendTemperature, "Temp task", 2048, NULL, 1, &xTaskTemperatureHandle);

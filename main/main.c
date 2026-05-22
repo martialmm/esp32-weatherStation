c666b41daf9e2c0aa6420bac7e44409fbcaa1ddf
Mars <petitmonne39@hotmail.fr>
2026-05-22 18:24:18 +0200

    REFACTOR: remove useless variables

--- main/main.c
+++ main/main.c
@@ -7,21 +7,17 @@
 
 void app_main(void)
 {
-    BaseType_t bluetoothInitStatus = initBluetooth();
-    BaseType_t displayInitStatus = initDisplay();
-    BaseType_t sensorsInitStatus = initSensors();
-
-    if(bluetoothInitStatus != pdPASS){
+    if(initBluetooth() != pdPASS){
         ESP_LOGE("Bluetooth", "Bluetooth Error!");
         return;
     }
 
-    if(displayInitStatus != pdPASS){
+    if(initDisplay() != pdPASS){
         ESP_LOGE("display", "Display Error!");
         return;
     }
 
-    if(sensorsInitStatus != pdPASS){
+    if(initSensors() != pdPASS){
         ESP_LOGE("sensors", "Sensors Error!");
         return;
     }
